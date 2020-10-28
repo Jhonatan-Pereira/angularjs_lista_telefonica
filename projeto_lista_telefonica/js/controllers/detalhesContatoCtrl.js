f977c4ced7c378653d9e1d933a8ba8642e44aa3a
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-28 14:20:16 -0300

    interceptors timestamp, error e loading

--- projeto_lista_telefonica/js/controllers/detalhesContatoCtrl.js
+++ projeto_lista_telefonica/js/controllers/detalhesContatoCtrl.js
@@ -1,5 +1,5 @@
 angular.module("listaTelefonica").controller("detalhesContatoCtrl", function ($scope, $routeParams, contato) {
-  console.log($routeParams.id)
+  // console.log($routeParams.id)
 
   $scope.contato = contato.data;
 });
